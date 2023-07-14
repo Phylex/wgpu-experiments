2eece5cec75e56c4b6715f5a28b624a15a5f9e5b
Alexande Becker <alexander.becker42@gmail.com>
2023-07-14 19:02:33 +0800

    removed dead code

--- src/lib.rs
+++ src/lib.rs
@@ -178,11 +178,6 @@ impl State {
         );
 
         let light = light::Light::new(&mut device);
-
-        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-            label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
-        });
         
         let depth_texture = texture::Texture::create_depth_texture(&device, &config, "depth texture");
 
